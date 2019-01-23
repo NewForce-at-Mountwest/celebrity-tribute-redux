92f9375d604669f207aec715a15ebc3b46380f76
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-01-23 11:44:58 -0500

    added readme and basic data structure for lin data

---
+++ README.md
@@ -0,0 +1,5 @@
+# Celebrity Tribute - JavaScript Edition 
+
+### This is the boilerplate code for rebuilding the Celebrity Tribute Group Project with JavaScript.
+
+
---
+++ celebrity.js
@@ -0,0 +1,36 @@
+const linData = {
+    newsfeed: [
+        {
+            title: "",
+            url: "",
+            date: "",
+        }
+    ],
+    personalLife: {
+        birthDate: "",
+        birthLocation: "",
+        cityOfResidence: "",
+        family: {
+            spouse: "",
+            kids: [],
+            parents: [],
+            pets: []
+        }
+    },
+    career: {
+        shortIntro: "",
+        playsWritten: [],
+        roles: [],
+        notableSongs: [],
+        awards: []
+    },
+    executiveSummary: {
+        knownCollaborations: [],
+        image: {
+            photURL: "",
+            caption: ""
+        },
+        listOfAliases: [],
+        countryOfResidence: ""
+    }
+}
