1aec82fc173c1798dbf19c29301326a27978670d
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-01-25 15:55:47 -0500

    added news articles to data

--- celebrity.js
+++ celebrity.js
@@ -1,10 +1,36 @@
 const linData = {
     newsfeed: [
         {
-            title: "",
-            url: "",
-            date: "",
+            title: "Lin Manuel Miranda Working on Moana Sequel",
+            url: "https://www.cinemablend.com/news/2465663/is-lin-manuel-miranda-working-on-a-moana-sequel",
+            date: "1/23/19"
+        },
+        {
+            title: "Lin-Manuel Miranda is peeved about this year’s Oscars",
+            url: "https://pagesix.com/2019/01/25/lin-manuel-miranda-is-peeved-about-this-years-oscars/",
+            date: "1/25/19",
+        },
+        {
+            title: "Lin-Manuel Miranda in Talks to Write Disney's First Latina Disney Princess",
+            url: "https://www.thedailybeast.com/lin-manuel-miranda-in-talks-to-write-disneys-first-latina-princess",
+            date: "1/25/19",
+        },
+        {
+            title: "Puerto Rico Gives Lin-Manuel Miranda a Hero's Welcome at Hamilton Opening Weekend: 'I Felt It'",
+            url: "  https://people.com/theater/lin-manuel-miranda-hamilton-puerto-rico-standing-ovation/",
+            date: "1/25/19"
+        },
+        {
+            title: "See Lin-Manuel Miranda, ‘Hamilton’ Cast Perform ‘The Story of Tonight’ With Jimmy Fallon",
+            url: "  https://www.rollingstone.com/tv/tv-news/lin-manuel-miranda-hamilton-jimmy-fallon-779743/",
+            date: "1/16/19"
+        },
+        {
+            title: "Lin-Manuel Miranda And 'Hamilton' Partners Save A Manhattan Theater Bookstore",
+            url: "https://www.npr.org/2019/01/08/683284098/lin-manuel-miranda-and-hamilton-partners-save-a-manhattan-theater-bookstore",
+            date: "1/8/19"
         }
+        
     ],
     personalLife: {
         birthDate: "",
