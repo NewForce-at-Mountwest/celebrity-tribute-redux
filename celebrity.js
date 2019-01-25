264d6e45844850ad6e3d7e0616c24738822abec9
colonelsped <joshua.lee.joseph@gmail.com>
2019-01-25 15:56:16 -0500

    added career info

--- celebrity.js
+++ celebrity.js
@@ -18,11 +18,11 @@ const linData = {
         }
     },
     career: {
-        shortIntro: "",
-        playsWritten: [],
-        roles: [],
-        notableSongs: [],
-        awards: []
+        shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
+        MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical" ],
+        notableRoles: ["Usnavi", "Alexander Hamilton"],
+        notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
+        awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
     },
     executiveSummary: {
         knownCollaborations: [],
